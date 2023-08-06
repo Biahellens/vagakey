92abce5eb6ead7630f70f0cfe39a17c4e0b9528f
Bianca Hellen <biancahellen03@icloud.com>
2023-08-06 10:25:01 -0300

    create header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import clsx from 'clsx'
 import styles from './header.module.scss'
+import vk from '../../assets/icons/vkIcon.svg'
 
 function Header() {
   return (
-    <div>
-      <body>
-        <div><p>oi</p></div>
-      </body>
+    <div className={styles.header}>
+      <div className={styles.items}>
+        <div className={styles.content}>
+          <div className={styles.img}>
+            <img src={vk} alt="vaga-key" />
+          </div>
+          <div className={styles.links}>
+            <div className={clsx(styles.link, [styles.actualPage])}>
+              <p>Home</p>
+            </div>
+            <div className={clsx(styles.link, [styles.pages])}>
+              <p>Aplicativo</p>
+            </div>
+            <div className={clsx(styles.link, [styles.pages])}>
+              <p>Mapa de Vagas</p>
+            </div>
+            <div className={clsx(styles.link, [styles.pages])}>
+              <p>Custos</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.login}>
+          <a>
+            <button type='submit'>
+              <p>Login</p>
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from './home.module.scss'
+import { Header } from '../../components/index'
 
 function Home() {
   return (
     <div>
+      <Header />
       <body>
         <div><p>oi</p></div>
       </body>
